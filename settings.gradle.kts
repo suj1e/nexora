d6adc774b54c870529d967536caead77b547e87c
suj1e <13156660820@163.com>
2026-01-29 09:16:45 +0800

    chore: update code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,6 @@
 rootProject.name = "nexora-spring-boot-starters"
 
 include(
-    "nexora-dependencies",
-    "nexora-spring-boot-starter-all",
-    "nexora-spring-boot-starter-id",
     "nexora-spring-boot-starter-web",
     "nexora-spring-boot-starter-redis",
     "nexora-spring-boot-starter-kafka",
